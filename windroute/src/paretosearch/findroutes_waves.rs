69c307d126c3725c8d200eadee6390bc7f20d13e
albetam <albetam@dtu.dk>
2025-07-08 20:38:33 +0200

    Using assert to check for invalid values (infinity) instead of if-else block with manual error.

--- windroute/src/paretosearch/findroutes_waves.rs
+++ windroute/src/paretosearch/findroutes_waves.rs
@@ -3,7 +3,7 @@
 //
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 // This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-// You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
+// You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 use crate::paretosearch;
 
@@ -43,74 +43,91 @@ pub fn generate(
         for next_index in 0..graph[current].len() {
             for t_current in 0..time_steps {
                 if cost[current][t_current].is_finite() {
-                    if weather_twa[current][t_current].is_finite()
-                        && weather_tws[current][t_current].is_finite()
-                    {
-                        let next: usize = graph[current][next_index];
-                        let twa: f64 =
-                            paretosearch::weather(weather_twa, lng, lat, current, next, t_current);
-                        let tws: f64 = weather_tws[current][t_current];
-                        let iwd: f64 =
-                            paretosearch::weather(weather_wd, lng, lat, current, next, t_current);
-                        let hs: f64 = weather_hs[current][t_current];
-                        for mode_index in 0..modes_speed.len() {
-                            let (energy_kwh, duration, _speed) = sail_arc(
-                                &modes_speed[mode_index],
-                                &modes_power[mode_index],
-                                lng[current],
-                                lat[current],
-                                lng[next],
-                                lat[next],
-                                twa_coords
-                                    .iter()
-                                    .enumerate()
-                                    .map(|(i, &a)| (i, (a - twa).abs()))
-                                    .min_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
-                                    .map(|(i, _)| i)
-                                    .unwrap(),
-                                tws_coords
-                                    .iter()
-                                    .enumerate()
-                                    .map(|(i, &a)| (i, (a - tws).abs()))
-                                    .min_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
-                                    .map(|(i, _)| i)
-                                    .unwrap(),
-                                iwd_coords
-                                    .iter()
-                                    .enumerate()
-                                    .map(|(i, &a)| (i, (a - iwd).abs()))
-                                    .min_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
-                                    .map(|(i, _)| i)
-                                    .unwrap(),
-                                hs_coords
-                                    .iter()
-                                    .enumerate()
-                                    .map(|(i, &a)| (i, (a - hs).abs()))
-                                    .min_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
-                                    .map(|(i, _)| i)
-                                    .unwrap(),
-                                stop_time[current],
-                            );
-                            if duration.is_finite() {
-                                let t_next: usize =
-                                    (((time_resolution * (t_current as f64) + duration)
-                                        / time_resolution)
-                                        .round() as usize)
-                                        .min(time_steps - 1);
-                                if is_reachable[next][t_next] {
-                                    if cost[current][t_current] + energy_kwh < cost[next][t_next]
-                                        && cost[current][t_current] + energy_kwh < max_energy
-                                    {
-                                        cost[next][t_next] = cost[current][t_current] + energy_kwh;
-                                        pred_place[next][t_next] = current;
-                                        pred_time[next][t_next] = t_current;
-                                        mode[next][t_next] = mode_index;
-                                    }
+                    assert!(
+                        weather_twa[current][t_current].is_finite(),
+                        "Needs weather twa different from infinity at index [{}, {}]",
+                        current,
+                        t_current
+                    );
+                    assert!(
+                        weather_tws[current][t_current].is_finite(),
+                        "Needs weather tws different from infinity at index [{}, {}]",
+                        current,
+                        t_current
+                    );
+                    assert!(
+                        weather_wd[current][t_current].is_finite(),
+                        "Needs weather wd different from infinity at index [{}, {}]",
+                        current,
+                        t_current
+                    );
+                    assert!(
+                        weather_hs[current][t_current].is_finite(),
+                        "Needs weather hs different from infinity at index [{}, {}]",
+                        current,
+                        t_current
+                    );
+                    let next: usize = graph[current][next_index];
+                    let twa: f64 =
+                        paretosearch::weather(weather_twa, lng, lat, current, next, t_current);
+                    let tws: f64 = weather_tws[current][t_current];
+                    let iwd: f64 =
+                        paretosearch::weather(weather_wd, lng, lat, current, next, t_current);
+                    let hs: f64 = weather_hs[current][t_current];
+                    for mode_index in 0..modes_speed.len() {
+                        let (energy_kwh, duration, _speed) = sail_arc(
+                            &modes_speed[mode_index],
+                            &modes_power[mode_index],
+                            lng[current],
+                            lat[current],
+                            lng[next],
+                            lat[next],
+                            twa_coords
+                                .iter()
+                                .enumerate()
+                                .map(|(i, &a)| (i, (a - twa).abs()))
+                                .min_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
+                                .map(|(i, _)| i)
+                                .unwrap(),
+                            tws_coords
+                                .iter()
+                                .enumerate()
+                                .map(|(i, &a)| (i, (a - tws).abs()))
+                                .min_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
+                                .map(|(i, _)| i)
+                                .unwrap(),
+                            iwd_coords
+                                .iter()
+                                .enumerate()
+                                .map(|(i, &a)| (i, (a - iwd).abs()))
+                                .min_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
+                                .map(|(i, _)| i)
+                                .unwrap(),
+                            hs_coords
+                                .iter()
+                                .enumerate()
+                                .map(|(i, &a)| (i, (a - hs).abs()))
+                                .min_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
+                                .map(|(i, _)| i)
+                                .unwrap(),
+                            stop_time[current],
+                        );
+                        if duration.is_finite() {
+                            let t_next: usize = (((time_resolution * (t_current as f64) + duration)
+                                / time_resolution)
+                                .round() as usize)
+                                .min(time_steps - 1);
+                            if is_reachable[next][t_next] {
+                                if cost[current][t_current] + energy_kwh < cost[next][t_next]
+                                    && cost[current][t_current] + energy_kwh < max_energy
+                                {
+                                    cost[next][t_next] = cost[current][t_current] + energy_kwh;
+                                    pred_place[next][t_next] = current;
+                                    pred_time[next][t_next] = t_current;
+                                    mode[next][t_next] = mode_index;
                                 }
                             }
                         }
-                    } else {
-                        unimplemented!("Need weather twa and weather tws different form infinity");
                     }
                 }
             }
